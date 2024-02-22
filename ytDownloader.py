74371cac884401d3b0eca26530ca47a2aeb05aba
Priyanka Soni <Priyankasoni2016@gmail.com>
2024-02-22 12:26:48 +0530

    ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -5,6 +5,24 @@ try :
 
    yt = Youtube(url)
 
+   print("Title:", yt.title)
+   print("Views:", yt.Views)
+
+   # Get the highest resolution stream
+
+   yd = yt.streams.get_highest_resoluiton()
+
+   # Download the video to the current directory
+
+   yd.download()
+
+   print("Download Complete.")
+
+   except Exception as e:
+
+      print("An error occured:", str(e))
+
+
    
 
 
